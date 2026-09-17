3b2699c64d11807ea91848d938ca201c83291455
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-09-17 14:06:53 +0530

    Add authentication routes and ensure User model is reused if already defined

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -33,6 +34,7 @@ mongoose.connect(process.env.MONGO_URI).then(
 
 
 
+app.use("/api/auth", authRoutes);
 
 
 app.listen(process.env.PORT || 5000, 
--- models/User.js
+++ models/User.js
@@ -76,6 +76,6 @@ userSchema.methods.comparePassword = async function (enteredPassword) {
   return bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
