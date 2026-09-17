cd6548e2966cac6b4cb96e4f59d6606434cc3a29
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-09-17 13:22:32 +0530

    corrected import error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-import User from "../models/user.js";
+import User from "../models/User.js";
 import generateToken from "../utils/generateToken.js";
 
 export const registerUser = async (req, res) => {
